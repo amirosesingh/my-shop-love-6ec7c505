7e828cd238777cfb9bd4bd2a05813e6c3b9920dd
amirose singh <amirosesingh@gmail.com>
2026-09-25 19:43:02 +0800

    Fix SQL Server default repair syntax

--- database/sqlserver/schema.sql
+++ database/sqlserver/schema.sql
@@ -9752,7 +9752,8 @@ IF OBJECT_ID(N'dbo.activity_events', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.acti
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_0 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[activity_events] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_0, N']', N']]') + N']');
+    DECLARE @legacy_json_default_0_sql nvarchar(max) = N'ALTER TABLE dbo.[activity_events] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_0);
+    EXEC sys.sp_executesql @legacy_json_default_0_sql;
     ALTER TABLE dbo.[activity_events] ADD CONSTRAINT [DF_activity_events_meta] DEFAULT (N'{}') FOR [meta];
   END;
 END;
@@ -9766,7 +9767,8 @@ IF OBJECT_ID(N'dbo.bookings', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.bookings', 
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_1 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[bookings] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_1, N']', N']]') + N']');
+    DECLARE @legacy_json_default_1_sql nvarchar(max) = N'ALTER TABLE dbo.[bookings] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_1);
+    EXEC sys.sp_executesql @legacy_json_default_1_sql;
     ALTER TABLE dbo.[bookings] ADD CONSTRAINT [DF_bookings_charges] DEFAULT (N'{}') FOR [charges];
   END;
 END;
@@ -9780,7 +9782,8 @@ IF OBJECT_ID(N'dbo.cashiers', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.cashiers', 
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_2 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[cashiers] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_2, N']', N']]') + N']');
+    DECLARE @legacy_json_default_2_sql nvarchar(max) = N'ALTER TABLE dbo.[cashiers] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_2);
+    EXEC sys.sp_executesql @legacy_json_default_2_sql;
     ALTER TABLE dbo.[cashiers] ADD CONSTRAINT [DF_cashiers_permissions] DEFAULT (N'{}') FOR [permissions];
   END;
 END;
@@ -9794,7 +9797,8 @@ IF OBJECT_ID(N'dbo.integration_settings', N'U') IS NOT NULL AND COL_LENGTH(N'dbo
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_3 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[integration_settings] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_3, N']', N']]') + N']');
+    DECLARE @legacy_json_default_3_sql nvarchar(max) = N'ALTER TABLE dbo.[integration_settings] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_3);
+    EXEC sys.sp_executesql @legacy_json_default_3_sql;
     ALTER TABLE dbo.[integration_settings] ADD CONSTRAINT [DF_integration_settings_api_keys_encrypted] DEFAULT (N'{}') FOR [api_keys_encrypted];
   END;
 END;
@@ -9808,7 +9812,8 @@ IF OBJECT_ID(N'dbo.payment_transactions', N'U') IS NOT NULL AND COL_LENGTH(N'dbo
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_4 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[payment_transactions] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_4, N']', N']]') + N']');
+    DECLARE @legacy_json_default_4_sql nvarchar(max) = N'ALTER TABLE dbo.[payment_transactions] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_4);
+    EXEC sys.sp_executesql @legacy_json_default_4_sql;
     ALTER TABLE dbo.[payment_transactions] ADD CONSTRAINT [DF_payment_transactions_metadata] DEFAULT (N'{}') FOR [metadata];
   END;
 END;
@@ -9822,7 +9827,8 @@ IF OBJECT_ID(N'dbo.pos_settings', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.pos_set
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_5 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_5, N']', N']]') + N']');
+    DECLARE @legacy_json_default_5_sql nvarchar(max) = N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_5);
+    EXEC sys.sp_executesql @legacy_json_default_5_sql;
     ALTER TABLE dbo.[pos_settings] ADD CONSTRAINT [DF_pos_settings_fonts] DEFAULT (N'{}') FOR [fonts];
   END;
 END;
@@ -9836,7 +9842,8 @@ IF OBJECT_ID(N'dbo.pos_settings', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.pos_set
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_6 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_6, N']', N']]') + N']');
+    DECLARE @legacy_json_default_6_sql nvarchar(max) = N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_6);
+    EXEC sys.sp_executesql @legacy_json_default_6_sql;
     ALTER TABLE dbo.[pos_settings] ADD CONSTRAINT [DF_pos_settings_qr] DEFAULT (N'{}') FOR [qr];
   END;
 END;
@@ -9850,7 +9857,8 @@ IF OBJECT_ID(N'dbo.pos_settings', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.pos_set
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_7 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_7, N']', N']]') + N']');
+    DECLARE @legacy_json_default_7_sql nvarchar(max) = N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_7);
+    EXEC sys.sp_executesql @legacy_json_default_7_sql;
     ALTER TABLE dbo.[pos_settings] ADD CONSTRAINT [DF_pos_settings_integration_settings] DEFAULT (N'{}') FOR [integration_settings];
   END;
 END;
@@ -9864,7 +9872,8 @@ IF OBJECT_ID(N'dbo.pos_settings', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.pos_set
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_8 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_8, N']', N']]') + N']');
+    DECLARE @legacy_json_default_8_sql nvarchar(max) = N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_8);
+    EXEC sys.sp_executesql @legacy_json_default_8_sql;
     ALTER TABLE dbo.[pos_settings] ADD CONSTRAINT [DF_pos_settings_booking_slip] DEFAULT (N'{}') FOR [booking_slip];
   END;
 END;
@@ -9878,7 +9887,8 @@ IF OBJECT_ID(N'dbo.pos_settings', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.pos_set
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_9 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_9, N']', N']]') + N']');
+    DECLARE @legacy_json_default_9_sql nvarchar(max) = N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_9);
+    EXEC sys.sp_executesql @legacy_json_default_9_sql;
     ALTER TABLE dbo.[pos_settings] ADD CONSTRAINT [DF_pos_settings_notification_settings] DEFAULT (N'{}') FOR [notification_settings];
   END;
 END;
@@ -9892,7 +9902,8 @@ IF OBJECT_ID(N'dbo.pos_settings', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.pos_set
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_10 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_10, N']', N']]') + N']');
+    DECLARE @legacy_json_default_10_sql nvarchar(max) = N'ALTER TABLE dbo.[pos_settings] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_10);
+    EXEC sys.sp_executesql @legacy_json_default_10_sql;
     ALTER TABLE dbo.[pos_settings] ADD CONSTRAINT [DF_pos_settings_receipt_design] DEFAULT (N'{}') FOR [receipt_design];
   END;
 END;
@@ -9906,7 +9917,8 @@ IF OBJECT_ID(N'dbo.products', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.products', 
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_11 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[products] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_11, N']', N']]') + N']');
+    DECLARE @legacy_json_default_11_sql nvarchar(max) = N'ALTER TABLE dbo.[products] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_11);
+    EXEC sys.sp_executesql @legacy_json_default_11_sql;
     ALTER TABLE dbo.[products] ADD CONSTRAINT [DF_products_stock_by_store] DEFAULT (N'{}') FOR [stock_by_store];
   END;
 END;
@@ -9920,7 +9932,8 @@ IF OBJECT_ID(N'dbo.settings_overrides', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.s
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_12 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[settings_overrides] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_12, N']', N']]') + N']');
+    DECLARE @legacy_json_default_12_sql nvarchar(max) = N'ALTER TABLE dbo.[settings_overrides] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_12);
+    EXEC sys.sp_executesql @legacy_json_default_12_sql;
     ALTER TABLE dbo.[settings_overrides] ADD CONSTRAINT [DF_settings_overrides_patch] DEFAULT (N'{}') FOR [patch];
   END;
 END;
@@ -9934,7 +9947,8 @@ IF OBJECT_ID(N'dbo.staff_roles', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.staff_ro
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_13 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[staff_roles] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_13, N']', N']]') + N']');
+    DECLARE @legacy_json_default_13_sql nvarchar(max) = N'ALTER TABLE dbo.[staff_roles] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_13);
+    EXEC sys.sp_executesql @legacy_json_default_13_sql;
     ALTER TABLE dbo.[staff_roles] ADD CONSTRAINT [DF_staff_roles_permissions] DEFAULT (N'{}') FOR [permissions];
   END;
 END;
@@ -9948,7 +9962,8 @@ IF OBJECT_ID(N'dbo.authorization_actions', N'U') IS NOT NULL AND COL_LENGTH(N'db
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_14 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[authorization_actions] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_14, N']', N']]') + N']');
+    DECLARE @legacy_json_default_14_sql nvarchar(max) = N'ALTER TABLE dbo.[authorization_actions] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_14);
+    EXEC sys.sp_executesql @legacy_json_default_14_sql;
     ALTER TABLE dbo.[authorization_actions] ADD CONSTRAINT [DF_authorization_actions_authority_limits] DEFAULT (N'{}') FOR [authority_limits];
   END;
 END;
@@ -9962,7 +9977,8 @@ IF OBJECT_ID(N'dbo.authorization_actions', N'U') IS NOT NULL AND COL_LENGTH(N'db
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_15 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[authorization_actions] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_15, N']', N']]') + N']');
+    DECLARE @legacy_json_default_15_sql nvarchar(max) = N'ALTER TABLE dbo.[authorization_actions] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_15);
+    EXEC sys.sp_executesql @legacy_json_default_15_sql;
     ALTER TABLE dbo.[authorization_actions] ADD CONSTRAINT [DF_authorization_actions_extra_authority] DEFAULT (N'{}') FOR [extra_authority];
   END;
 END;
@@ -9976,7 +9992,8 @@ IF OBJECT_ID(N'dbo.authorization_actions', N'U') IS NOT NULL AND COL_LENGTH(N'db
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_16 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[authorization_actions] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_16, N']', N']]') + N']');
+    DECLARE @legacy_json_default_16_sql nvarchar(max) = N'ALTER TABLE dbo.[authorization_actions] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_16);
+    EXEC sys.sp_executesql @legacy_json_default_16_sql;
     ALTER TABLE dbo.[authorization_actions] ADD CONSTRAINT [DF_authorization_actions_absolute_ceilings] DEFAULT (N'{}') FOR [absolute_ceilings];
   END;
 END;
@@ -9990,7 +10007,8 @@ IF OBJECT_ID(N'dbo.authorization_requests', N'U') IS NOT NULL AND COL_LENGTH(N'd
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_17 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[authorization_requests] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_17, N']', N']]') + N']');
+    DECLARE @legacy_json_default_17_sql nvarchar(max) = N'ALTER TABLE dbo.[authorization_requests] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_17);
+    EXEC sys.sp_executesql @legacy_json_default_17_sql;
     ALTER TABLE dbo.[authorization_requests] ADD CONSTRAINT [DF_authorization_requests_payload] DEFAULT (N'{}') FOR [payload];
   END;
 END;
@@ -10004,7 +10022,8 @@ IF OBJECT_ID(N'dbo.authorization_requests', N'U') IS NOT NULL AND COL_LENGTH(N'd
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_18 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[authorization_requests] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_18, N']', N']]') + N']');
+    DECLARE @legacy_json_default_18_sql nvarchar(max) = N'ALTER TABLE dbo.[authorization_requests] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_18);
+    EXEC sys.sp_executesql @legacy_json_default_18_sql;
     ALTER TABLE dbo.[authorization_requests] ADD CONSTRAINT [DF_authorization_requests_approved_payload] DEFAULT (N'{}') FOR [approved_payload];
   END;
 END;
@@ -10018,7 +10037,8 @@ IF OBJECT_ID(N'dbo.authorization_requests', N'U') IS NOT NULL AND COL_LENGTH(N'd
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_19 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[authorization_requests] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_19, N']', N']]') + N']');
+    DECLARE @legacy_json_default_19_sql nvarchar(max) = N'ALTER TABLE dbo.[authorization_requests] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_19);
+    EXEC sys.sp_executesql @legacy_json_default_19_sql;
     ALTER TABLE dbo.[authorization_requests] ADD CONSTRAINT [DF_authorization_requests_bill_snapshot] DEFAULT (N'{}') FOR [bill_snapshot];
   END;
 END;
@@ -10032,7 +10052,8 @@ IF OBJECT_ID(N'dbo.authorization_log', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.au
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_20 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[authorization_log] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_20, N']', N']]') + N']');
+    DECLARE @legacy_json_default_20_sql nvarchar(max) = N'ALTER TABLE dbo.[authorization_log] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_20);
+    EXEC sys.sp_executesql @legacy_json_default_20_sql;
     ALTER TABLE dbo.[authorization_log] ADD CONSTRAINT [DF_authorization_log_detail] DEFAULT (N'{}') FOR [detail];
   END;
 END;
@@ -10046,7 +10067,8 @@ IF OBJECT_ID(N'dbo.record_edits', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.record_
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_21 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[record_edits] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_21, N']', N']]') + N']');
+    DECLARE @legacy_json_default_21_sql nvarchar(max) = N'ALTER TABLE dbo.[record_edits] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_21);
+    EXEC sys.sp_executesql @legacy_json_default_21_sql;
     ALTER TABLE dbo.[record_edits] ADD CONSTRAINT [DF_record_edits_before_value] DEFAULT (N'{}') FOR [before_value];
   END;
 END;
@@ -10060,7 +10082,8 @@ IF OBJECT_ID(N'dbo.record_edits', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.record_
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_22 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[record_edits] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_22, N']', N']]') + N']');
+    DECLARE @legacy_json_default_22_sql nvarchar(max) = N'ALTER TABLE dbo.[record_edits] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_22);
+    EXEC sys.sp_executesql @legacy_json_default_22_sql;
     ALTER TABLE dbo.[record_edits] ADD CONSTRAINT [DF_record_edits_after_value] DEFAULT (N'{}') FOR [after_value];
   END;
 END;
@@ -10074,7 +10097,8 @@ IF OBJECT_ID(N'dbo.record_edits', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.record_
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_23 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[record_edits] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_23, N']', N']]') + N']');
+    DECLARE @legacy_json_default_23_sql nvarchar(max) = N'ALTER TABLE dbo.[record_edits] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_23);
+    EXEC sys.sp_executesql @legacy_json_default_23_sql;
     ALTER TABLE dbo.[record_edits] ADD CONSTRAINT [DF_record_edits_stock_deltas] DEFAULT (N'{}') FOR [stock_deltas];
   END;
 END;
@@ -10088,7 +10112,8 @@ IF OBJECT_ID(N'dbo.shift_close_events', N'U') IS NOT NULL AND COL_LENGTH(N'dbo.s
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_24 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[shift_close_events] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_24, N']', N']]') + N']');
+    DECLARE @legacy_json_default_24_sql nvarchar(max) = N'ALTER TABLE dbo.[shift_close_events] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_24);
+    EXEC sys.sp_executesql @legacy_json_default_24_sql;
     ALTER TABLE dbo.[shift_close_events] ADD CONSTRAINT [DF_shift_close_events_detail] DEFAULT (N'{}') FOR [detail];
   END;
 END;
@@ -10102,7 +10127,8 @@ IF OBJECT_ID(N'dbo.entity_status_history', N'U') IS NOT NULL AND COL_LENGTH(N'db
       AND REPLACE(REPLACE(REPLACE(dc.definition,N'(',N''),N')',N''),N' ',N'')=N'N''[]'''
   );
   IF @legacy_json_default_25 IS NOT NULL BEGIN
-    EXEC(N'ALTER TABLE dbo.[entity_status_history] DROP CONSTRAINT [' + REPLACE(@legacy_json_default_25, N']', N']]') + N']');
+    DECLARE @legacy_json_default_25_sql nvarchar(max) = N'ALTER TABLE dbo.[entity_status_history] DROP CONSTRAINT ' + QUOTENAME(@legacy_json_default_25);
+    EXEC sys.sp_executesql @legacy_json_default_25_sql;
     ALTER TABLE dbo.[entity_status_history] ADD CONSTRAINT [DF_entity_status_history_metadata] DEFAULT (N'{}') FOR [metadata];
   END;
 END;
@@ -10113,8 +10139,10 @@ IF OBJECT_ID(N'dbo.authorization_requests', N'U') IS NOT NULL AND COL_LENGTH(N'd
     JOIN sys.columns c ON c.object_id=dc.parent_object_id AND c.column_id=dc.parent_column_id
     WHERE dc.parent_object_id=OBJECT_ID(N'dbo.authorization_requests') AND c.name=N'requested_amount'
   );
-  IF @legacy_requested_amount_default IS NOT NULL
-    EXEC(N'ALTER TABLE dbo.[authorization_requests] DROP CONSTRAINT [' + REPLACE(@legacy_requested_amount_default, N']', N']]') + N']');
+  IF @legacy_requested_amount_default IS NOT NULL BEGIN
+    DECLARE @legacy_requested_amount_default_sql nvarchar(max) = N'ALTER TABLE dbo.[authorization_requests] DROP CONSTRAINT ' + QUOTENAME(@legacy_requested_amount_default);
+    EXEC sys.sp_executesql @legacy_requested_amount_default_sql;
+  END;
   ALTER TABLE dbo.[authorization_requests] ALTER COLUMN [requested_amount] decimal(38,12) NULL;
 END;
 
